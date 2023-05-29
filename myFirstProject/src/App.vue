cf2492dcf5ac0d56d1ffdbae297966579ee0954b
heeyeon <dlgmldus3289@naver.com>
2023-05-29 13:15:41 +0900

    F [MOD] modal component

---
+++ myFirstProject/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <ModalWindow
+    :rooms="rooms"
+    :modalOpen="modalOpen"
+    @closeModal="modalOpen = false"
+    :index="index"
+  />
+
+  <!-- 메뉴목록 -->
+  <div class="menu">
+    <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
+  </div>
+
+  <!--원룸 목록-->
+  <div v-for="(i, key) in rooms" :key="key">
+    <img :src="i.image" />
+    <h4 @click="clickHandler(), (index = key)">{{ i.title }}</h4>
+    <p>{{ i.price }}</p>
+  </div>
+</template>
+
+<script>
+import 원룸데이터 from "./data/data.js";
+import ModalWindow from "./components/ModalWindow.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      menus: ["Home", "Shop", "About"],
+      modalOpen: false,
+      rooms: 원룸데이터,
+      index: 0,
+    };
+  },
+  methods: {
+    clickHandler() {
+      this.modalOpen = true;
+    },
+  },
+  components: {
+    ModalWindow,
+  },
+};
+</script>
+
+<style scoped="scoped">
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+img {
+  width: 100%;
+}
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+</style>
---
+++ myFirstProject/src/components/ModalWindow.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="black-bg" v-if="modalOpen == true">
+    <div class="white-bg">
+      <img :src="rooms[index].image" />
+      <h4>{{ rooms[index].title }}</h4>
+      <p>{{ rooms[index].content }}</p>
+      <button @click="$emit('closeModal')">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modalOpen: Boolean,
+    rooms: Object,
+    index: Number,
+  },
+};
+</script>
+
+<style>
+img {
+  width: 100%;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
