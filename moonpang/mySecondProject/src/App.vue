bbb2230e030caccca9dddfc48b2fa255e230a6f1
M00NPANG <moonsky1124@naver.com>
2023-06-11 16:38:04 +0900

    MP [ADD] my second project

---
+++ moonpang/mySecondProject/src/App.vue
@@ -0,0 +1,118 @@
+<template>
+
+  <div class="menu">
+    <a v-for="(메뉴, i) in 메뉴들" :key="메뉴">{{ 메뉴들[i] }}</a>
+  </div>
+
+  <ModalView
+    v-if="모달창열렸니[currentIndex] == true"
+    :datas="원룸들"
+    :index="this.currentIndex"
+    :boolean="모달창열렸니"
+    @childEvent="handleEvent($event)">
+  </ModalView>
+
+  <button class="btn" @click="namesort()">이름</button>
+  <button class="btn" @click="pricesort()">가격순</button>
+  <button>복구</button>
+  <div v-for="(item, i) in 원룸들" :key="i">
+    <img :src="item.image" class="room-img" />
+    <h1 @click="clickHandler(i, item)">{{ item.title }}</h1>
+    <p>{{ item.price }}원</p>
+  </div>
+</template>
+
+<script>
+import 원룸데이터 from "./data/data";
+import ModalView from "./components/ModalView.vue";
+
+export default {
+  name: "App",
+  components: {
+    ModalView,
+},
+  data() {
+    return {
+      메뉴들: ["Home", "Shop", "About"],
+      currentIndex: 0,
+      currentItem: {},
+      모달창열렸니: [false, false, false, false, false, false],
+      원룸들: 원룸데이터,
+      원래데이터: 원룸데이터,
+    };
+  },
+  methods: {
+    clickHandler(i, item) {
+      this.currentItem = item;
+      this.currentIndex = i;
+      this.모달창열렸니[this.currentIndex] = true;
+    },
+    handleEvent(event) {
+      console.log(event);
+      console.log(event.result);
+      if (event.result) {
+        this.모달창열렸니[this.currentIndex] = false
+      }
+    },
+    namesort(){
+      this.원룸들 =[...this.원래데이터].sort((a,b)=>{
+        if(a.title > b.title) return 1;
+        if(a.title < b.title) return -1;
+        return 0;
+      }) 
+    },
+    pricesort(){
+      this.원룸들 = [...this.원래데이터].sort((a,b) => a.price - b.price)
+    }
+  },
+};
+</script>
+
+<style>
+.room-img {
+  widows: 100%;
+  margin-top: 40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
---
+++ moonpang/mySecondProject/src/components/ModalView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="black-bg">
+    <div class="white-bg">
+      <img :src="this.datas[this.index].image" class="room-img" />
+      <h4>{{ this.datas[this.index].title }}</h4>
+      <p>{{ this.datas[index].content }}</p>
+      <input  type="text" v-model.number="data">
+      <p>입력값 {{ data }}</p>
+      <button @click="$emit('childEvent', { result: true })">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    datas: Object,
+    index: Number,
+    boolean: Boolean,
+  },
+  name: "App",
+  data(){
+    return {
+      data:  "",
+    }
+  },
+  methods:{  
+  },
+  watch:{
+    data(data){
+      if (data <100)
+      alert("100안넘")
+    }
+  }
+};
+</script>
+
+<style>
+.room-img {
+  widows: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
